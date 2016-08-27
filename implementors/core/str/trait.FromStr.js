19702761e60cab7ef27e28b92f95f1ded10fb894
azyobuzin_bot <gh-bot@azyobuzi.net>
2016-08-27 18:16:21 +0000

    9ab57e3f3e626eb06b2cd29d5541c3c842b2d948

--- implementors/core/str/trait.FromStr.js
+++ implementors/core/str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["rustc_serialize"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='enum' href='rustc_serialize/json/enum.Json.html' title='rustc_serialize::json::Json'>Json</a>",];implementors["url"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='url/struct.Url.html' title='url::Url'>Url</a>",];
+implementors["libc"] = [];implementors["url"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='url/struct.Url.html' title='url::Url'>Url</a>",];implementors["rustc_serialize"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='enum' href='rustc_serialize/json/enum.Json.html' title='rustc_serialize::json::Json'>Json</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
